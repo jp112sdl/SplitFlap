8344bfc6a6e1fdabed87daeadfd13fb399f390f9
jp112sdl <jp112sdl@icloud.com>
2020-03-16 08:41:56 +0100

    update

--- Code/SplitFlap/SplitFlap.ino
+++ Code/SplitFlap/SplitFlap.ino
@@ -1,28 +1,33 @@
+// Magnet rote Seite gegen A3144 schmale Seite
+
+
 #include <AccelStepper.h>
 
-#define BUSY_LED                LED_BUILTIN
-#define STEPPER_COUNT           8
+#define BUSY_LED                A5
+#define STEPPER_COUNT           10
 #define NUM_FLAPS               45
 #define STEPS_PER_ROUND         4096
 #define STEPS_PER_FLAP          (STEPS_PER_ROUND * 1.0 / NUM_FLAPS * 1.0)
 #define SPEED                   1000.0
 #define ACCELERATION            10000.0
 
-const uint8_t SENSOR_PINS[STEPPER_COUNT] = { A0 , 0, 0, 0, 0, 0, 0, 0 };
-const uint8_t ZERO_OFFSET[STEPPER_COUNT] = { 100, 0, 0, 0, 0, 0, 0, 0 };
+const uint8_t SENSOR_PINS[STEPPER_COUNT] = {  A0 ,  A2  ,  A4 ,  A6 ,  A8 , A10 , A12 , A14 ,  A1  , A3 };
+const uint8_t ZERO_OFFSET[STEPPER_COUNT] = { 100 ,  140 ,  170 , 180 , 180 , 180 , 130 , 180 , 170 , 140 };
 
 AccelStepper steppers[STEPPER_COUNT] = {
-  AccelStepper(AccelStepper::HALF4WIRE,  4,  6,  5,  7),
-  AccelStepper(AccelStepper::HALF4WIRE,  0,  0,  0,  0),
-  AccelStepper(AccelStepper::HALF4WIRE,  0,  0,  0,  0),
-  AccelStepper(AccelStepper::HALF4WIRE,  0,  0,  0,  0),
-  AccelStepper(AccelStepper::HALF4WIRE,  0,  0,  0,  0),
-  AccelStepper(AccelStepper::HALF4WIRE,  0,  0,  0,  0),
-  AccelStepper(AccelStepper::HALF4WIRE,  0,  0,  0,  0),
-  AccelStepper(AccelStepper::HALF4WIRE,  0,  0,  0,  0)
+  AccelStepper(AccelStepper::HALF4WIRE,   4,  3,  2,  5),
+  AccelStepper(AccelStepper::HALF4WIRE,   8,  7,  6,  9),
+  AccelStepper(AccelStepper::HALF4WIRE,  12, 11, 10, 13),
+  AccelStepper(AccelStepper::HALF4WIRE,  20, 19, 18, 21),
+  AccelStepper(AccelStepper::HALF4WIRE,  24, 23, 22, 25),
+  AccelStepper(AccelStepper::HALF4WIRE,  28, 27, 26, 29),
+  AccelStepper(AccelStepper::HALF4WIRE,  42, 41, 40, 43), 
+  AccelStepper(AccelStepper::HALF4WIRE,  46, 45, 44, 47),
+  AccelStepper(AccelStepper::HALF4WIRE,  38, 37, 36, 39),
+  AccelStepper(AccelStepper::HALF4WIRE,  34, 33, 32, 35)
 };
 
-bool running[STEPPER_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0};
+bool running[STEPPER_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 String serialInput = "";
 //                                                     ÄÖÜ ß
@@ -47,7 +52,12 @@ void gotoZero(uint8_t moduleNum, bool disable) {
 
   Serial.print("gotoZero("); Serial.print(moduleNum); Serial.print(","), Serial.print(disable); Serial.println(")");
   steppers[moduleNum].enableOutputs();
-  bool zeroPos =  (digitalRead(SENSOR_PINS[moduleNum]) == 0);
+  bool zeroPos = (digitalRead(SENSOR_PINS[moduleNum]) == 0);
+  while (zeroPos) {
+    zeroPos = (digitalRead(SENSOR_PINS[moduleNum]) == 0);
+    steppers[moduleNum].runSpeed();
+  }
+  
   while (!zeroPos) {
     zeroPos = (digitalRead(SENSOR_PINS[moduleNum]) == 0);
     if (zeroPos) {
@@ -64,8 +74,9 @@ void gotoZero(uint8_t moduleNum, bool disable) {
 
 void gotoZeroAll() {
   for (uint8_t j = 0; j < STEPPER_COUNT; j++) {
-    gotoZero(j, false);
+    gotoZero(j, true);
   }
+  Serial.println("gotoZeroAll -> Finish!");
 }
 
 void gotoLetter(char printletter, uint8_t moduleNum) {
@@ -121,20 +132,22 @@ bool processFlapRun() {
 
 void setup() {
   Serial.begin(57600);
+  Serial3.begin(57600);
 
   initHW();
 
-  gotoZero(0, true);
-  //gotoZeroAll();
-
+  //gotoZero(7, true);
+  gotoZeroAll();
 }
 
 void loop() {
   bool busy = processFlapRun();
 
   bool newChar = false;
-  if (!busy && Serial.available() > 0) {
-    char in = Serial.read();
+  if (!busy && (Serial.available() > 0 || Serial1.available() > 0)) {
+
+    char in = (Serial.available() > 0) ? Serial.read() : Serial3.read();
+
     serialInput += in;
 
     if (in == '\n') {
@@ -145,7 +158,8 @@ void loop() {
   if (newChar) {
     newChar = false;
     if (serialInput.length() == (STEPPER_COUNT + 1)) {
-      for (uint8_t i = 0; i < STEPPER_COUNT; i++){
+      serialInput.toUpperCase();
+      for (uint8_t i = 0; i < STEPPER_COUNT; i++) {
         char letter = serialInput[i];
         gotoLetter(letter, i);
       }
