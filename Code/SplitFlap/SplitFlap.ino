765f4e314c7df75656142d27ebc28a366620651a
jp112sdl <jp112sdl@icloud.com>
2020-03-19 19:30:57 +0100

    update

--- Code/SplitFlap/SplitFlap.ino
+++ Code/SplitFlap/SplitFlap.ino
@@ -11,8 +11,8 @@
 #define SPEED                   1000.0
 #define ACCELERATION            10000.0
 
-const uint8_t SENSOR_PINS[STEPPER_COUNT] = {  A0 ,  A2  ,  A4 ,  A6 ,  A8 , A10 , A12 , A14 ,  A1  , A3 };
-const uint8_t ZERO_OFFSET[STEPPER_COUNT] = { 100 ,  140 ,  170 , 180 , 180 , 180 , 130 , 180 , 170 , 140 };
+const uint8_t SENSOR_PINS[STEPPER_COUNT] = {  A0 ,  A2  ,  A4 ,  A6 ,  A8  , A10 , A12 , A14 ,  A1  , A3 };
+const uint8_t ZERO_OFFSET[STEPPER_COUNT] = { 110 ,  100 ,  110 , 170 , 190 , 100 , 120 , 180 , 165 , 95 };
 
 AccelStepper steppers[STEPPER_COUNT] = {
   AccelStepper(AccelStepper::HALF4WIRE,   4,  3,  2,  5),
@@ -47,34 +47,42 @@ void initHW() {
   pinMode(BUSY_LED, OUTPUT);
 }
 
-void gotoZero(uint8_t moduleNum, bool disable) {
+void setBusyLED(bool s) {
+  digitalWrite(BUSY_LED, s);
+}
+
+void gotoZero(uint8_t moduleNum) {
+  setBusyLED(true);
   TWBR = 2;  // 12 = 400 kHz; 2 = 800 kHz
 
-  Serial.print("gotoZero("); Serial.print(moduleNum); Serial.print(","), Serial.print(disable); Serial.println(")");
+  Serial.print("gotoZero("); Serial.print(moduleNum);Serial.print(")");
   steppers[moduleNum].enableOutputs();
+  steppers[moduleNum].setSpeed(SPEED);
   bool zeroPos = (digitalRead(SENSOR_PINS[moduleNum]) == 0);
   while (zeroPos) {
     zeroPos = (digitalRead(SENSOR_PINS[moduleNum]) == 0);
     steppers[moduleNum].runSpeed();
   }
-  
-  while (!zeroPos) {
+  unsigned long zeroPosStartMillis = millis();
+  while (!zeroPos && millis() - zeroPosStartMillis < 8000) {
     zeroPos = (digitalRead(SENSOR_PINS[moduleNum]) == 0);
     if (zeroPos) {
-      Serial.println("Zero Pos");
+      Serial.println("  Zero Pos detected");
       steppers[moduleNum].setCurrentPosition(0);
       steppers[moduleNum].runToNewPosition(ZERO_OFFSET[moduleNum]);
     }
     steppers[moduleNum].runSpeed();
   }
+  if (!zeroPos) Serial.println(" TIMEOUT!");
   steppers[moduleNum].setCurrentPosition(0);
-  Serial.print("setCurrentPosition(0) : ("); Serial.print(moduleNum); Serial.print(","), Serial.print(disable); Serial.println(")");
-  if (disable) steppers[moduleNum].disableOutputs();
+  //Serial.print("setCurrentPosition(0) : ("); Serial.print(moduleNum); Serial.print(","), Serial.print(disable); Serial.println(")");
+  steppers[moduleNum].disableOutputs();
+  setBusyLED(false);
 }
 
 void gotoZeroAll() {
   for (uint8_t j = 0; j < STEPPER_COUNT; j++) {
-    gotoZero(j, true);
+    gotoZero(j);
   }
   Serial.println("gotoZeroAll -> Finish!");
 }
@@ -104,30 +112,29 @@ void gotoLetter(char printletter, uint8_t moduleNum) {
 }
 
 bool processFlapRun() {
-  bool isBusy = false;
-  for (uint8_t j = 0; j < STEPPER_COUNT; j++) {
-    if (steppers[j].distanceToGo() != 0 && !running[j]) {
-      Serial.print("Enable  Steppers ("); Serial.print(j, DEC); Serial.println(")");
-      steppers[j].enableOutputs();
-      running[j] = true;
-    }
-
-    steppers[j].run();
-
-    if (steppers[j].distanceToGo() == 0 && running[j]) {
-      Serial.print("Disable Steppers ("); Serial.print(j, DEC); Serial.println(")");
-      if (steppers[j].currentPosition() > STEPS_PER_ROUND) {
-        steppers[j].setCurrentPosition(steppers[j].currentPosition() - STEPS_PER_ROUND);
+    bool isBusy = false;
+    for (uint8_t j = 0; j < STEPPER_COUNT; j++) {
+      if (steppers[j].distanceToGo() != 0 && !running[j]) {
+        Serial.print("Enable  Steppers ("); Serial.print(j, DEC); Serial.println(")");
+        steppers[j].enableOutputs();
+        running[j] = true;
       }
-      steppers[j].disableOutputs();
-      running[j] = false;
-    }
 
-    if (running[j]) lastmillis = millis();
-    if (running[j]) isBusy = true;
-  }
-  digitalWrite(BUSY_LED, isBusy);
-  return isBusy;
+      steppers[j].run();
+
+      if (steppers[j].distanceToGo() == 0 && running[j]) {
+        Serial.print("Disable Steppers ("); Serial.print(j, DEC); Serial.println(")");
+        if (steppers[j].currentPosition() > STEPS_PER_ROUND) {
+          steppers[j].setCurrentPosition(steppers[j].currentPosition() - STEPS_PER_ROUND);
+        }
+        steppers[j].disableOutputs();
+        running[j] = false;
+      }
+
+      if (running[j]) lastmillis = millis();
+      if (running[j]) isBusy = true;
+    }
+    return isBusy;
 }
 
 void setup() {
@@ -136,12 +143,13 @@ void setup() {
 
   initHW();
 
-  //gotoZero(7, true);
   gotoZeroAll();
 }
 
 void loop() {
   bool busy = processFlapRun();
+
+  setBusyLED(busy);
 
   bool newChar = false;
   if (!busy && (Serial.available() > 0 || Serial1.available() > 0)) {
@@ -157,6 +165,13 @@ void loop() {
 
   if (newChar) {
     newChar = false;
+
+    if (serialInput.length() == 3) {
+      if (serialInput[0] == 'z') {
+        gotoZero(serialInput[1] - 48);
+      }
+    }
+
     if (serialInput.length() == (STEPPER_COUNT + 1)) {
       serialInput.toUpperCase();
       for (uint8_t i = 0; i < STEPPER_COUNT; i++) {
